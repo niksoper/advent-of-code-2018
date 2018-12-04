25be27018405f5d5d2d2392dded18246a5610da2
niksoper <niksoper@hotmail.com>
2018-12-04 19:55:03 +0000

    Day 3, part 1

--- day03/part1.js
+++ day03/part1.js
@@ -1,21 +1,41 @@
-const input = [
-  '#1299 @ 621,742: 13x11',
-  '#1300 @ 746,592: 27x19',
-  '#1301 @ 551,561: 14x29',
-  '#1302 @ 563,818: 11x29',
-]
+const readFile = require('../input').readInput
+const input = readFile('./input.txt')
 
 function parseRow(row) {
   const [id, coords] = row.split('@').map(s => s.trim())
   const [position, size] = coords.split(':').map(s => s.trim())
   const [x, y] = position.split(',').map(Number)
   const [w, h] = size.split('x').map(Number)
-  const right = x + w
+  const right = x + (w-1)
   const left = x
   const top = y
-  const bottom = y + h
+  const bottom = y + (h-1)
 
   return { id, x, y , w, h, top, right, bottom, left }
 }
 
-const patches = input.map(parseRow)
+function markCloth(patch, cloth, overlap) {
+  const mark = {}
+  for (let i = patch.left; i <= patch.right; i++) {
+    for (let j = patch.top; j <= patch.bottom; j++) {
+      let key = `${i},${j}`
+      if (cloth[key]) {
+        overlap[key] = true
+      }
+      cloth[key] = true
+    }
+  }
+}
+
+const result = input.reduce((acc, row) => {
+  const patch = parseRow(row)
+  markCloth(patch, acc.cloth, acc.overlap)
+  
+  return {
+    patches: [ ...acc.patches, patch],
+    overlap: acc.overlap,
+    cloth: acc.cloth
+  }
+}, { patches: [], cloth: {}, overlap: {}})
+
+console.log(Object.keys(result.overlap).length)
